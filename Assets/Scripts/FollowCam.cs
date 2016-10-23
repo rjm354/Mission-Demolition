1b23e8f0cfa6bbb06a1819ea21fd518584f1aa05
rjm354 <rjm354@rutgers.edu>
2016-10-23 14:33:51 -0400

    Adjusting orthographicSize
    FollowCam has been adjusted, will zoom outward to show the projectile
    and its height. The projectiles bouce.

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+    static public FollowCam S; //a FollowCam Singleton
+
+    //fields set in the Unity Inspector pane
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool _____________________;
+
+    //fields set dynamically
+    public GameObject poi; //the point of interest
+    public float camZ; //the desired Z pos of the camera
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+	    //if there's only one line of code following an if, it doesn't need braces
+        if(poi == null)
+        {
+            return; //return if there is no poi
+        }
+        //get the position of the poi
+        Vector3 destination = poi.transform.position;
+        //Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        //Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        //retain a destination.z of camZ
+        destination.z = camZ;
+        //set the camera to the destination
+        transform.position = destination;
+        //set the orthographicSize of the Camera to keep Ground in view
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+	}
+}
--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Slingshot : MonoBehaviour {
     //fields set in the Unity Inspector pane
     public GameObject prefabProjectile;
+    public float velocityMult = 4f;
     public bool __________;
     //fields set dynamically
     public GameObject launchPoint;
@@ -14,6 +15,41 @@ public class Slingshot : MonoBehaviour {
     {
        
 	}
+    void Update()
+    {
+        //If Slingshot is not in aimingMode, don't run this code
+        if(!aimingMode)
+        {
+            return;
+        }
+        //Get the current mouse position in 2D screen coordinates
+        Vector3 mousePos2D = Input.mousePosition;
+        //Covert the mouse position to 3D world coordinates
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        //Find the delta from the launchPos to the mousePos3D
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        //Limit mouseDelta to the radius of the Slingshot SphereCollider
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if(mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        //Move the projectile to this new position
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            //The mouse has been released
+            aimingMode = false;
+            projectile.GetComponent<Rigidbody>().isKinematic = false;
+            projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+            FollowCam.S.poi = projectile;
+            projectile = null;
+        }
+    }
     void Awake()
     {
         Transform launchPointTrans = transform.FindChild("LaunchPoint");
